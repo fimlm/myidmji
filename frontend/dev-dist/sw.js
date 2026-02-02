e7e6699cfeba3e1eafa5fcfd6b07c17eb4ae492c
AO by Moratoo, Inc. <angel@moratoo.com>
2026-02-02 17:35:50 +0100

    chore: Update service worker precache revision for index.html.

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6pedfbq9cvk"
+    "revision": "0.1ccq7meim74"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
